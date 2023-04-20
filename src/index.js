40d6abb5295ad7204c120f2b4ffd41b74fbe99fa
Erik <emirkovic@student.unipu.hr>
2023-04-20 18:06:36 +0200

    Changes for server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import axios from 'axios';
+import './index.css';
 
-const baseURL = 'http://localhost:9000';
-
-axios.defaults.baseURL = baseURL;
+axios.defaults.baseURL = process.env.REACT_APP_API_BASE_URL;
 axios.defaults.headers.common['Authorization'] = localStorage.getItem('JWT_PAYLOAD');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -17,7 +14,3 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
